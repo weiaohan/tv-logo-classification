6691d10c647f9d355bff264dea9335beaab43922
weiaohan <380629654@qq.com>
2018-12-15 22:02:34 +0800

    优化代码结构

--- train.py
+++ train.py
@@ -1,47 +1,12 @@
 # -*- coding: UTF-8 -*- 
 import numpy as np
 from keras.preprocessing.image import ImageDataGenerator
-from keras.models import Sequential
-from keras.layers.normalization import BatchNormalization
-from keras.layers import Dense, Flatten, Dropout, Convolution2D, MaxPooling2D, Activation
-from keras import applications
 from keras.preprocessing import image
-from keras.models import load_model
 import time
 import matplotlib.pyplot as plt
+import build_model
 
-
-model = Sequential()
-model.add(Convolution2D(32, 3, 3, input_shape=(200, 60, 3)))
-model.add(Activation("relu"))
-#model.add(BatchNormalization(axis=-1))
-model.add(MaxPooling2D(pool_size=(3, 3)))
-model.add(Dropout(0.25))
-
-model.add(Convolution2D(64, 3, 3))
-model.add(Activation("relu"))
-model.add(Convolution2D(64, 3, 3))
-model.add(Activation("relu"))
-#model.add(BatchNormalization(axis=-1))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-
-model.add(Convolution2D(128, 3, 3))
-model.add(Activation("relu"))
-model.add(Convolution2D(128, 3, 3))
-model.add(Activation("relu"))
-#model.add(BatchNormalization(axis=-1))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-
-model.add(Flatten())
-model.add(Dense(1024))
-model.add(Activation("relu"))
-#model.add(BatchNormalization(axis=-1))
-model.add(Dropout(0.5))
-model.add(Dense(9))
-model.add(Activation("softmax"))
-
+model = build_model.build_model()
 # model.load_weights('first_try.h5')
 
 model.compile(optimizer="rmsprop", loss="categorical_crossentropy", metrics=["accuracy"])
