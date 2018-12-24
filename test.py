fdbd4e6c868f919caa191883cb7fd24cd2e76368
weiaohan <380629654@qq.com>
2018-12-24 22:15:59 +0800

    新增权重值

--- test.py
+++ test.py
@@ -6,11 +6,11 @@ from PIL import Image,ImageFont,ImageDraw
 import build_model
 import classfication_dict
 
-model = build_model.build_model(3, 3, (200, 60, 3), 30)
-model.load_weights("train_weights/2018-12-21.h5")
+model = build_model.build_model(3, 3, (150, 150, 3), 30)
+model.load_weights("train_weights/2018-12-21-2.h5")
 
-img_path = "test_pic/cctv1-test.png"
-img = image.load_img(img_path, target_size=(200,60))
+img_path = "test_pic/hubei-t.png"
+img = image.load_img(img_path, target_size=(150,150))
 x = image.img_to_array(img)
 x = np.expand_dims(x, axis=0)
 preds = model.predict(x, batch_size=1)
